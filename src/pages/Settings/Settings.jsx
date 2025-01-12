8e6ef3ffaac691ed60035fb343d55f9e9986904c
Beridze0 <beridzedavit904@gmail.com>
2025-01-12 23:06:51 +0400

    added new structure to settings

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -42,38 +42,16 @@ const Settings = () => {
             </div>
             <div className='account-settings-channel'>
                 <h1>Your YouTube channel</h1>
-                <p>This is your public presence on YouTube. You need a channel to upload your own videos, comment on videos, or create playlists.</p>
+                <p className='account-settings-small-txt'>This is your public presence on YouTube. You need a channel to upload your own videos, comment on videos, or create playlists.</p>
                 <div className='account-settings-create-channel'>
                     <p>Your channel</p>
-                    <button>Create a channel</button>
+                    <button className='settings-create-channel-btn'>Create a channel</button>
                 </div>
             </div>
             <div className='account-settings-your-account'>
                 <h1>Your account</h1>
-                <p>You sign in to YouTube with your Google Account</p>
-                <div className='settings-your-account'>
-                    <p>Google Account</p>
-                    <div className='settings-your-account-item'>
-                        <span>View or change your Google Account settings</span>
-                        <p>You will be redirected to your Google Account page</p>
-                    </div>
-                </div>
-                <div className='settings-your-account-item'>
-                    <p>Family Center</p>
-                    <div className='account-settings-actions'>
-                        <span>Manage kids profiles and features for teens</span>
-                        <p>Tools to connect parents, kids, and teens on YouTube</p>
-                    </div>
-                </div>
-                <div className='settings-your-account-item'>
-                    <p>Google Account</p>
-                    <div className='account-settings-actions'>
-                        <p><span className='membership-status'>No membership</span>
-                            <span>Get YouTube Premium</span>
-                        </p>
-                        <p>YouTube Premium offers uninterrupted music, ad-free videos, and more</p>
-                    </div>
-                </div>
+                <p className='account-settings-small-txt'>You sign in to YouTube with your Google Account</p>
+                
             </div>
         </div>
     </div>
