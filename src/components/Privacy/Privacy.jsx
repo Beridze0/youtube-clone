e68c1605593c69cea5f128964d37fe47ea2163f9
Beridze0 <beridzedavit904@gmail.com>
2025-01-13 18:28:06 +0400

    added privacy structure

--- src/components/Privacy/Privacy.jsx
+++ src/components/Privacy/Privacy.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Privacy = () => {
   return (
-    <div>Privacy</div>
+    <div className='privacy-container'>
+        <div>
+            <h1>Privacy</h1>
+            <p>Manage what you share on YouTube</p>
+            <p className='settings-small-txt'>Choose who can see your subscriptions</p>
+        </div>
+    </div>
   )
 }
 
